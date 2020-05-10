70487281b7b820ff5fc86820d15fd2a14f1110d9
Stan Reduta <stanislaw.reduta@gmail.com>
2020-05-10 17:37:10 +0200

    Adds `setup.py`

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='airpress',
+    version='0.0.1',
+    author='Stan Reduta',
+    author_email='stanislaw.reduta@gmail.com',
+    description='A frustration-free compression tool for PKPass archives.',
+    long_description_content_type='text/markdown',
+    license='MIT',
+    url='https://github.com/captain-fox/airpress',
+    packages=['airpress'],
+    install_requires=['cryptography>=2.9.2'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
